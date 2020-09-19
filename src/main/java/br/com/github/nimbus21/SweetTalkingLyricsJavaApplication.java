37752add309b0c453a18438d40c04bd1e10c1845
Nimbus21 <thandy_97@hotmail.com>
2020-09-19 04:54:37 -0300

    Initial commit

---
+++ src/main/java/br/com/github/nimbus21/SweetTalkingLyricsJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.nimbus21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetTalkingLyricsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetTalkingLyricsJavaApplication.class, args);
+	}
+
+}
